2a30a3a9e9c49c89bb823e678f4050cd586d9851
Alexandre Lairan <lairana@free.fr>
2019-02-25 12:15:45 +0100

    Add slime at click

--- src/state/game.cpp
+++ src/state/game.cpp
@@ -4,6 +4,7 @@
 
 #include "game.h"
 #include "../key_binding.h"
+#include <iostream>
 
 namespace hista {
     namespace state {
@@ -18,34 +19,52 @@ namespace hista {
             auto window = _ctx.window;
 
             window->draw(*_mario);
+            for (const auto &slime : _slimes) {
+                window->draw(*slime);
+            }
             window->draw(*_level);
         }
 
         bool game::update(sf::Time dt) {
             _mario->update(dt);
 
+            for (const auto &slime : _slimes) {
+                slime->update(dt);
+            }
+
             return false;
         }
 
         bool game::handle_event(const sf::Event &event) {
-            auto binding = key_binding{};
-            if (event.key.code == sf::Keyboard::Escape) {
-                request_stack_pop();
-                request_stack_push(ID::Menu);
+            if (event.type == sf::Event::KeyPressed) {
+                auto binding = key_binding{};
+                if (event.key.code == sf::Keyboard::Escape) {
+                    request_stack_pop();
+                    request_stack_push(ID::Menu);
+                }
+
+                if (event.key.code == sf::Keyboard::P) {
+                    request_stack_pop();
+                    request_stack_push(ID::Win);
+                }
+
+                if (event.key.code == sf::Keyboard::Q) {
+                    request_stack_pop();
+                    request_stack_push(ID::Lose);
+                }
+
+                for (const auto &action : binding.actions()) {
+                    _mario->start_movement(action);
+                }
             }
 
-            if (event.key.code == sf::Keyboard::P) {
-                request_stack_pop();
-                request_stack_push(ID::Win);
-            }
+            if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+                sf::Vector2i position = sf::Mouse::getPosition();
+                sf::Vector2f convert = sf::Vector2f(position.x - 1000, position.y);
 
-            if (event.key.code == sf::Keyboard::Q) {
-                request_stack_pop();
-                request_stack_push(ID::Lose);
-            }
-
-            for (const auto &action : binding.actions()) {
-                _mario->start_movement(action);
+                std::cerr << "Add slime at " << convert.x << " - " << convert.y << std::endl;
+                std::cerr << "Should be " << position.x << " - " << position.y << std::endl;
+                _slimes.push_back(entity::make_enemy("../assets/meta/enemy/slime.hista", convert));
             }
 
             return false;
